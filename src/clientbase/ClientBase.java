d48fe4ebc762527bfe507585c082339e34e59213
russo.salvatore <russo.salvatore@TAVE-PC27.OSDBSESTO.local>
2021-02-18 14:11:47 +0100

    rr

--- src/clientbase/ClientBase.java
+++ src/clientbase/ClientBase.java
@@ -7,6 +7,7 @@ package clientbase;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,28 +21,30 @@ public class ClientBase {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         System.out.println("Apertua connessione");
-        try {
-            while (true) {
-
-                Socket server = new Socket("127.0.0.1", 6666);
+        try { 
+                Socket server = new Socket("127.0.0.1", 5500);
                 InputStream dalServer = server.getInputStream();
                 BufferedReader lettore = new BufferedReader(
-                        new InputStreamReader(dalServer));
-
+                new InputStreamReader(dalServer));
+                OutputStream versoilserver=server.getOutputStream();
+                BufferedWriter scrittore=new BufferedWriter(new OutputStreamWriter(versoilserver));
+                String k=sc.next();
+                scrittore.write(k);
                 String risposta = lettore.readLine();
                 System.out.println("risposta del server: " + risposta);
-
                 lettore.close();
                 server.close();
                 System.out.println("chiusura connessione");
                 Thread.sleep(5000);
-            }
+            
         } catch (IOException ex) {
             Logger.getLogger(ClientBase.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InterruptedException e) {
 
         }
     }
-
 }
+
+
